f9f20aba2de41a57dafd7c684682d141cc438cbc
khadijahnabil <nabilkhadijah7@gmail.com>
2022-10-27 16:34:14 +0100

    fixed warning in navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,11 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <nav>
-      <img src="../images/pokemon-logo.png" className="pokemon--logo" />
+      <img
+        src="../images/pokemon-logo.png"
+        alt="pokemon-logo"
+        className="pokemon--logo"
+      />
     </nav>
   );
 }
