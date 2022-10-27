d6c90ac5f5d7a1f56d0222296a0a525cfe7ce9b2
khadijahnabil <nabilkhadijah7@gmail.com>
2022-10-27 16:18:43 +0100

    created header!

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <img src="../images/pokemon-logo.png" className="pokemon--logo" />
+    </header>
+  );
+}
