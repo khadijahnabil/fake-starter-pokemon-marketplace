c117004b508baa11b4d13f3eaa964a60f4fa9cd6
khadijahnabil <nabilkhadijah7@gmail.com>
2022-10-27 17:30:25 +0100

    added hero component and border

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 
 import Navbar from "./Navbar";
+import Hero from "./Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <h1>Welcome to our Starter Pokemon Marketplace!</h1>
+    </section>
+  );
+}
