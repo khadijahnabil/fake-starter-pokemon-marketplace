c22e8b6266bb390ac48cf46404a4a91b80344f45
khadijahnabil <nabilkhadijah7@gmail.com>
2022-10-31 19:26:49 +0100

    created pokemonData.js

---
+++ src/pokemonData.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    img: "../images/bulbasaur.png",
+    title: "Bulbasaur",
+    price: 20,
+    stats: {
+      type: "Grass/Poison",
+      species: "Seed Pokemon",
+      height: 0.7,
+      weight: 6.9,
+      abilities: "Overgrow",
+    },
+    stock: 3,
+  },
+  {
+    id: 2,
+    img: "../images/squirtle.png",
+    title: "Squirtle",
+    price: 25,
+    stats: {
+      type: "Water",
+      species: "Tiny Turtle Pokemon",
+      height: 0.5,
+      weight: 9.0,
+      abilities: "Torrent",
+    },
+    stock: 0,
+  },
+  {
+    id: 3,
+    img: "../images/charmander.png",
+    title: "Charmander",
+    price: 25,
+    stats: {
+      type: "Fire",
+      species: "Lizard Pokemon",
+      height: 0.6,
+      weight: 8.5,
+      abilities: "Blaze",
+    },
+    stock: 1,
+  },
+];
